a8ee22386dd3b175f58315e7085db24bd44ae18b
zohaejaz95 <zohaejaz@live.com>
2022-01-26 22:24:42 +0500

    BR05: Add toCart Design Complete

--- components/Location.js
+++ components/Location.js
@@ -1,143 +1,143 @@
-import {
-  View,
-  Text,
-  PermissionsAndroid,
-  Platform,
-  Button,
-  Image,
-  StyleSheet,
-} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import Geolocation from '@react-native-community/geolocation';
+// import {
+//   View,
+//   Text,
+//   PermissionsAndroid,
+//   Platform,
+//   Button,
+//   Image,
+//   StyleSheet,
+// } from 'react-native';
+// import React, {useState, useEffect} from 'react';
+// import Geolocation from '@react-native-community/geolocation';
 
-const Location = () => {
-  const [currentLongitude, setCurrentLongitude] = useState('...');
-  const [currentLatitude, setCurrentLatitude] = useState('...');
-  const [locationStatus, setLocationStatus] = useState('');
-  useEffect(() => {
-    const requestLocationPermission = async () => {
-      if (Platform.OS === 'ios') {
-        getOneTimeLocation();
-        subscribeLocationLocation();
-      } else {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title: 'Location Access Required',
-              message: 'This App needs to Access your location',
-            },
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            //To Check, If Permission is granted
-            getOneTimeLocation();
-            subscribeLocationLocation();
-          } else {
-            setLocationStatus('Permission Denied');
-          }
-        } catch (err) {
-          console.warn(err);
-        }
-      }
-    };
-    requestLocationPermission();
-    return () => {
-      Geolocation.clearWatch(watchID);
-    };
-  }, []);
+// const Location = () => {
+//   const [currentLongitude, setCurrentLongitude] = useState('...');
+//   const [currentLatitude, setCurrentLatitude] = useState('...');
+//   const [locationStatus, setLocationStatus] = useState('');
+//   useEffect(() => {
+//     const requestLocationPermission = async () => {
+//       if (Platform.OS === 'ios') {
+//         getOneTimeLocation();
+//         subscribeLocationLocation();
+//       } else {
+//         try {
+//           const granted = await PermissionsAndroid.request(
+//             PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//             {
+//               title: 'Location Access Required',
+//               message: 'This App needs to Access your location',
+//             },
+//           );
+//           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//             //To Check, If Permission is granted
+//             getOneTimeLocation();
+//             subscribeLocationLocation();
+//           } else {
+//             setLocationStatus('Permission Denied');
+//           }
+//         } catch (err) {
+//           console.warn(err);
+//         }
+//       }
+//     };
+//     requestLocationPermission();
+//     return () => {
+//       Geolocation.clearWatch(watchID);
+//     };
+//   }, []);
 
-  const getOneTimeLocation = () => {
-    setLocationStatus('Getting Location ...');
-    Geolocation.getCurrentPosition(
-      //Will give you the current location
-      position => {
-        setLocationStatus('You are Here');
+//   const getOneTimeLocation = () => {
+//     setLocationStatus('Getting Location ...');
+//     Geolocation.getCurrentPosition(
+//       //Will give you the current location
+//       position => {
+//         setLocationStatus('You are Here');
 
-        //getting the Longitude from the location json
-        const currentLongitude = JSON.stringify(position.coords.longitude);
+//         //getting the Longitude from the location json
+//         const currentLongitude = JSON.stringify(position.coords.longitude);
 
-        //getting the Latitude from the location json
-        const currentLatitude = JSON.stringify(position.coords.latitude);
+//         //getting the Latitude from the location json
+//         const currentLatitude = JSON.stringify(position.coords.latitude);
 
-        //Setting Longitude state
-        setCurrentLongitude(currentLongitude);
+//         //Setting Longitude state
+//         setCurrentLongitude(currentLongitude);
 
-        //Setting Longitude state
-        setCurrentLatitude(currentLatitude);
-      },
-      error => {
-        setLocationStatus(error.message);
-      },
-      {
-        enableHighAccuracy: false,
-        timeout: 30000,
-        maximumAge: 1000,
-      },
-    );
-  };
-  return (
-    <View style={styles.container}>
-      <View style={styles.container}>
-        <Image
-          source={{
-            uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/location.png',
-          }}
-          style={{width: 100, height: 100}}
-        />
-        <Text style={styles.boldText}>{locationStatus}</Text>
-        <Text
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 16,
-          }}>
-          Longitude: {currentLongitude}
-        </Text>
-        <Text
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 16,
-          }}>
-          Latitude: {currentLatitude}
-        </Text>
-        <View style={{marginTop: 20}}>
-          <Button title="Button" onPress={getOneTimeLocation} />
-        </View>
-      </View>
-      <Text
-        style={{
-          fontSize: 18,
-          textAlign: 'center',
-          color: 'grey',
-        }}>
-        React Native Geolocation
-      </Text>
-      <Text
-        style={{
-          fontSize: 16,
-          textAlign: 'center',
-          color: 'grey',
-        }}>
-        www.aboutreact.com
-      </Text>
-    </View>
-  );
-};
+//         //Setting Longitude state
+//         setCurrentLatitude(currentLatitude);
+//       },
+//       error => {
+//         setLocationStatus(error.message);
+//       },
+//       {
+//         enableHighAccuracy: false,
+//         timeout: 30000,
+//         maximumAge: 1000,
+//       },
+//     );
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.container}>
+//         <Image
+//           source={{
+//             uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/location.png',
+//           }}
+//           style={{width: 100, height: 100}}
+//         />
+//         <Text style={styles.boldText}>{locationStatus}</Text>
+//         <Text
+//           style={{
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//             marginTop: 16,
+//           }}>
+//           Longitude: {currentLongitude}
+//         </Text>
+//         <Text
+//           style={{
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//             marginTop: 16,
+//           }}>
+//           Latitude: {currentLatitude}
+//         </Text>
+//         <View style={{marginTop: 20}}>
+//           <Button title="Button" onPress={getOneTimeLocation} />
+//         </View>
+//       </View>
+//       <Text
+//         style={{
+//           fontSize: 18,
+//           textAlign: 'center',
+//           color: 'grey',
+//         }}>
+//         React Native Geolocation
+//       </Text>
+//       <Text
+//         style={{
+//           fontSize: 16,
+//           textAlign: 'center',
+//           color: 'grey',
+//         }}>
+//         www.aboutreact.com
+//       </Text>
+//     </View>
+//   );
+// };
 
-export default Location;
+// export default Location;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boldText: {
-    fontSize: 25,
-    color: 'red',
-    marginVertical: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'white',
+//     padding: 10,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   boldText: {
+//     fontSize: 25,
+//     color: 'red',
+//     marginVertical: 16,
+//   },
+// });
